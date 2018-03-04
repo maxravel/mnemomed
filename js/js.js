26bf48adb0049d9a307de58a0fe64f393140146b
maxravel <maxmaterniak@gmail.com>
2018-03-04 20:01:14 +0100

    adding counter of good answers

--- js/js.js
+++ js/js.js
@@ -16,6 +16,7 @@ const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
 const loadInfo = document.querySelector('#loadInfo');
 let info = document.querySelector('#info');
+let punkty = document.querySelector('#punkty');
 
 problem.innerText = task1[0][0];
 answer1.innerText = task1[0][1];
@@ -30,6 +31,7 @@ answer3.style.backgroundColor = "white";
 answer4.style.backgroundColor = "white";
 answer5.style.backgroundColor = "white";
 
+let x=0;
 
 //*************************** NEXT SECTION *******************************
 
@@ -57,7 +59,7 @@ next.addEventListener('click', function(){
     answer5.style.backgroundColor = "white";
     info.innerText='';
     info.style.display='none';
-    
+
 });
 
 
@@ -88,12 +90,21 @@ prev.addEventListener('click', function(){
     
 });
 
+console.log(answer1.style.backgroundColor);
+
 
 //************************** ANSWER COLORS ****************************
 
 answer1.addEventListener('click', function(){
 
     if(task1[i][6]===0){
+        if(answer1.style.backgroundColor === "white" &&
+           answer2.style.backgroundColor === "white" &&
+           answer3.style.backgroundColor === "white" &&
+           answer4.style.backgroundColor === "white" &&
+           answer3.style.backgroundColor === "white"){
+            x++;
+        }
         answer1.style.backgroundColor = "green";
     }
 
@@ -101,23 +112,41 @@ answer1.addEventListener('click', function(){
         answer1.style.backgroundColor = "red";
     }
 
+    punkty.innerHTML = `Twoje punkty:  ${x}/${i+1}`;
+
 });
 
 answer2.addEventListener('click', function(){
 
     if(task1[i][6]===1){
+        if(answer1.style.backgroundColor === "white" &&
+        answer2.style.backgroundColor === "white" &&
+        answer3.style.backgroundColor === "white" &&
+        answer4.style.backgroundColor === "white" &&
+        answer3.style.backgroundColor === "white"){
+            x++;
+        }
         answer2.style.backgroundColor = "green";
     }
 
     else{
         answer2.style.backgroundColor = "red";
     }
+    console.log(x);
+    punkty.innerHTML = `Twoje punkty:  ${x}/${i+1}`;
 
 });
 
 answer3.addEventListener('click', function(){
 
     if(task1[i][6]===2){
+        if(answer1.style.backgroundColor === "white" &&
+        answer2.style.backgroundColor === "white" &&
+        answer3.style.backgroundColor === "white" &&
+        answer4.style.backgroundColor === "white" &&
+        answer3.style.backgroundColor === "white"){
+            x++;
+        }
         answer3.style.backgroundColor = "green";
     }
 
@@ -125,11 +154,20 @@ answer3.addEventListener('click', function(){
         answer3.style.backgroundColor = "red";
     }
 
+    punkty.innerHTML = `Twoje punkty:  ${x}/${i+1}`;
+
 });
 
 answer4.addEventListener('click', function(){
 
     if(task1[i][6]===3){
+        if(answer1.style.backgroundColor === "white" &&
+        answer2.style.backgroundColor === "white" &&
+        answer3.style.backgroundColor === "white" &&
+        answer4.style.backgroundColor === "white" &&
+        answer3.style.backgroundColor === "white"){
+            x++;
+        }
         answer4.style.backgroundColor = "green";
     }
 
@@ -137,18 +175,29 @@ answer4.addEventListener('click', function(){
         answer4.style.backgroundColor = "red";
     }
 
+    punkty.innerHTML = `Twoje punkty:  ${x}/${i+1}`;
+
 });
 
 answer5.addEventListener('click', function(){
 
     if(task1[i][6]===4){
+        if(answer1.style.backgroundColor === "white" &&
+        answer2.style.backgroundColor === "white" &&
+        answer3.style.backgroundColor === "white" &&
+        answer4.style.backgroundColor === "white" &&
+        answer3.style.backgroundColor === "white"){
+            x++;
+        }
         answer5.style.backgroundColor = "green";
     }
 
     else{
         answer5.style.backgroundColor = "red";
     }
-    
+
+    punkty.innerHTML = `Twoje punkty:  ${x}/${i+1}`;
+
 });
 
 
@@ -177,3 +226,7 @@ loadInfo.addEventListener('click', function(){
 
     info.innerText=task1[i][7];
 });
+
+
+
+console.log(x);
