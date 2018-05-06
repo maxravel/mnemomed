94064021c036045129db5a45255aeea52353bac2
maxravel <maxmaterniak@gmail.com>
2018-05-06 17:03:12 +0200

    counter works well

--- js/counter.js
+++ js/counter.js
@@ -1,16 +1,16 @@
-const pdays = document.querySelector("#days");
-const phours = document.querySelector("#hours");
-const pminutes = document.querySelector("#minutes");
-const pseconds = document.querySelector("#seconds");
-const title = document.querySelector("#counterDay");
+const odays = document.querySelector("#days");
+const ohours = document.querySelector("#hours");
+const ominutes = document.querySelector("#minutes");
+const oseconds = document.querySelector("#seconds");
+const otitle = document.querySelector("#counterDay");
 
 var xyz;
 
-function counter(){
+function counter(name, year, month, day, hour){
 
     const currentDate = new Date();
 
-    const ExamDate = new Date(2018,8,22,12);
+    const ExamDate = new Date(year,month,day,hour);
 
     var milisec = ExamDate-currentDate;
     var seconds = Math.floor(milisec/1000);
@@ -23,17 +23,20 @@ function counter(){
     var h = hours%=24; 
     var d = days;
 
-    pdays.textContent = d;
-    phours.textContent = h;
-    pminutes.textContent = m;
-    pseconds.textContent = s;
+    otitle.textContent = `Do ${name} pozostało:`;
+    odays.textContent = d;
+    ohours.textContent = h;
+    ominutes.textContent = m;
+    oseconds.textContent = s;
     
 }
 
-counter();
-xyz = setInterval('counter()', 1000);
+xyz = setInterval('counter("LEK 2018 jesień",2018,8,22,12)', 1000);
 
 
+
+//global scope for setInterval !!
+var abc;
 
 const changer = document.querySelector("#changeCount");
 
@@ -41,48 +44,24 @@ changer.addEventListener('click', finalTime);
 
 function finalTime(){
     
-    const pyear = document.querySelector("#year").value;
-    const pmonth = document.querySelector("#month").value;
-    const pday = document.querySelector("#day").value;
-    const phour = document.querySelector("#hour").value;   
-    let ExamDate = new Date(pyear,pmonth-1,pday,phour);
-    const ptitle = document.querySelector("#counterTitle").value;
-    console.log(ExamDate);
-
-    if(title==""||pyear==""||pmonth==""||pday==""){
+    const iyear = document.querySelector("#year").value;
+    const imonth = document.querySelector("#month").value;
+    const iday = document.querySelector("#day").value;
+    const ihour = document.querySelector("#hour").value;   
+    const ititle = document.querySelector("#counterTitle").value;
+    
+    clearInterval(abc);
+    if(ititle==""||iyear==""||imonth==""||iday==""){
         alert("Musisz wypełnic: nazwę, rok, miesiąc, dzień.");
     }
-    else if(pyear<2018||pyear>2100||pmonth<1||pmonth>12||pday<0||pday>31){
+    else if(iyear<2018||iyear>2100||imonth<1||imonth>12||iday<0||iday>31){
         alert("Wybierz rok poźniejszy niż 2017, wcześniejszy niż 2100, miesiąc z przedziału 1-12 i dzień z przedziału 1-31");
     }
     else{
-        let currentDate = new Date();
-
-        var milisec = ExamDate-currentDate;
-        var seconds = Math.floor(milisec/1000);
-        var minutes = Math.floor(seconds/60);
-        var hours = Math.floor(minutes/60);
-        var days = Math.floor(hours/24);
-
-        var s = seconds%=60;
-        var m = minutes%=60;
-        var h = hours%=24; 
-        var d = days;
-
-        pdays.textContent = d;
-        phours.textContent = h;
-        pminutes.textContent = m;
-        pseconds.textContent = s;
-        title.textContent = `Do ${ptitle} pozostało:`;
-
-        // pyear = "";
-        // pmonth = "";
-        // pday = "";
-        // phour = ""; 
-
 
         clearInterval(xyz);
-        setInterval('finalTime()', 1000);
+        
+        abc = setInterval(() => counter(ititle,iyear,imonth-1,iday,ihour), 1000);
         
     }
 }
