ae57575d89b7c4ae7f3ee2eb347d2c839854a476
maxravel <maxmaterniak@gmail.com>
2018-05-10 21:19:41 +0200

    pomodoro up 2

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,14 +1,33 @@
+//buttons
 const pomodoroB = document.querySelector("#pomodoroStart");
+const add5min = document.querySelector("#up5");
+const subtract5min = document.querySelector("#down5");
+
+//text
+const pomodoroT = document.querySelector("#pomodoroText");
+
 let pomodoroC = document.querySelector("#pomodoroCounter");
 let pomodoroCInt = parseInt(pomodoroC.textContent);
+if(localStorage.getItem("pomodoroValue")){
+    pomodoroC.textContent = localStorage.getItem("pomodoroValue")/60;
+    pomodoroCInt = localStorage.getItem("pomodoroValue")/60;
+}
+
 console.log(typeof pomodoroCInt);
 console.log(pomodoroCInt);
 
+//for global scope of Interval
 var pomodoroInterval;
+
+//button pomodoro listener
 pomodoroB.addEventListener("click", function(){
-pomodoroInterval = setInterval(countingWorks, 1000);
-pomodoroCInt*=60;
-pomodoroB.disabled = true;
+    pomodoroInterval = setInterval(countingWorks, 1000);
+    pomodoroCInt*=60;
+    pomodoroB.disabled = true;
+    add5min.disabled = true;
+    subtract5min.disabled = true; 
+    localStorage.setItem("pomodoroValue",pomodoroCInt);
+    pomodoroT.textContent = "Pracuj dzielnie!";
 
 function countingWorks(){
     if(pomodoroCInt%60<10){
@@ -24,13 +43,17 @@ function countingWorks(){
         clearInterval(pomodoroInterval);
         console.log("stop!!!");
         pomodoroB.disabled = false;
+        add5min.disabled = false;
+        subtract5min.disabled = false; 
+        pomodoroC.textContent = localStorage.getItem("pomodoroValue")/60;
+        pomodoroCInt = localStorage.getItem("pomodoroValue")/60;
+        pomodoroT.textContent = "Chwila przerwy i działaj dalej!";
     }
 };
 
 })
 
-const add5min = document.querySelector("#up5");
-const subtract5min = document.querySelector("#down5");
+
 
 add5min.addEventListener("click", function(){
     
