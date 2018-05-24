1aed8132f5b62fc733a6fae28c4b894998b4e708
maxravel <maxmaterniak@gmail.com>
2018-05-24 20:44:00 +0200

    curve up

--- js/curve.js
+++ js/curve.js
@@ -1,3 +1,5 @@
+let arrayTasks = [];
+
 function Task(title, type){
     this.title = title;
     this.type = type;
@@ -5,11 +7,30 @@ function Task(title, type){
 
 const curveA = document.querySelector("#curveAdd");
 const curveT = document.querySelector("#curveTask");
+const curveL = document.querySelector("#curveList");
 
 
 curveA.addEventListener("click", function(){
-    const d = new Task(`${curveT.value}`,1);
-    console.log(d);
-    let zxc = curveT.value;
-    console.log(zxc);
+    //creating new Task
+    const task = new Task(`${curveT.value}`,1);
+    localStorage.setItem(1,JSON.stringify(task));
+    console.log(task);
+    //adding new task to array
+    arrayTasks.push(task);
+    console.log(arrayTasks);
+    //creating UI of new task
+    let x = document.createElement("li");
+    x.textContent = task.title;
+    let del = document.createElement("a");
+    del.style.paddingLeft = "20px";
+    del.style.color = "red";
+
+    del.textContent = "x";
+    del.addEventListener("click", function(){
+        del.parentElement.remove();
+    })
+
+    x.appendChild(del);
+    curveL.appendChild(x);
+    
 });
