1a0b902052bb3531feb721b70e7953919ab1c223
maxravel <maxmaterniak@gmail.com>
2018-05-25 10:58:30 +0200

    working on curve functionality

--- js/curve.js
+++ js/curve.js
@@ -1,16 +1,21 @@
+//constructor of tasks
 function Task(title, type, timing){
     this.title = title;
-    this.type = type;
-    this.timing = timing;
+    this.type = type; //position 1 - 24h to review, position 2 - 72 h to review, position 3 - 1 week, position 4 - 1 month
+    this.timing = timing; //time of adding task
 }
 
-const curveA = document.querySelector("#curveAdd");
-const curveT = document.querySelector("#curveTask");
-const curveL = document.querySelector("#curveList");
+//selectors
+const curveA = document.querySelector("#curveAdd"); //button
+const curveT = document.querySelector("#curveTask"); //input
+const curveL = document.querySelector("#curveList"); //task ul
 
+
+//getting tasks from localStorage
 document.addEventListener('DOMContentLoaded',getTasks);
 
 function getTasks(){
+    let tasks =[];
     if(localStorage.getItem("tasks")===null){
         tasks = [];
     }
@@ -43,7 +48,7 @@ curveA.addEventListener("click", function(){
     }
     else{
         //creating new Task
-        const task = new Task(`${curveT.value}`,1,1);
+        const task = new Task(`${curveT.value}`,1,new Date());
         addToLocalStorage(task);
         //creating UI of new task
         let x = document.createElement("li");
@@ -80,12 +85,10 @@ function addToLocalStorage(task){
 function removeTask(e){
     if(e.target.parentElement.classList.contains('aaac')){
         e.target.parentElement.remove();
-
     }  
 
     //remove from Local Storage
     removeFromLocalStorage(e.target.parentElement);
-
 }
 
 function removeFromLocalStorage(x){
@@ -97,9 +100,34 @@ function removeFromLocalStorage(x){
         tasks=JSON.parse(localStorage.getItem('tasks'));
     }
     tasks.forEach(function(task, index){
-        if(x.textContent===(task.title+"x")){
+        // if(x.textContent===(task.title+"x")){
+        //     tasks.splice(index, 1);
+        // }
+
+        if(x.textContent.substr(0,x.textContent.length-1)===task.title){
             tasks.splice(index, 1);
         }
+        //console.log(x.textContent.substr(0,x.textContent.length-1));
     });
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
+
+function counting(x){
+    let tasks;
+    if(localStorage.getItem('tasks')===null){
+        tasks=[];
+    }
+    else{
+        tasks=JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.forEach(function(task){
+        if(x.textContent.substr(0,x.textContent.length-1)===task.title){
+            console.log(task.timing);
+        } 
+    });
+    console.log("timing");
+} 
+
+const dpr = document.querySelectorAll("li");
+console.log(dpr);
+dpr.forEach(function(x){x.addEventListener("click", counting)});
